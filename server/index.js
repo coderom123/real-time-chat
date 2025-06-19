8f2305c81d9bb5153c6b479fd1d09c266d07915f
coderom123 <161458704+coderom123@users.noreply.github.com>
2025-06-19 11:07:02 +0530

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: '*',
+  },
+});
+
+app.use(cors());
+
+io.on('connection', (socket) => {
+  console.log('User connected:', socket.id);
+
+  socket.on('sendMessage', (data) => {
+    io.emit('receiveMessage', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+server.listen(5000, () => {
+  console.log('Server running on port 5000');
+});
